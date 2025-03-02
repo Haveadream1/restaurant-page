1455cb0f14ea6d4e2371bf56bed8af22e9739f2a
Haveadream1 <260602david@gmail.com>
2025-03-02 08:34:29 +0100

    fix: remove clean as it delete my css/html

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true, // ensure that your output directory is cleaned before each build, making it easier to manage and debug your application
     },
     module: {
         rules: [

5f7b781a4d4a25db4926f89b88f04d0e6a3abd2e
Haveadream1 <260602david@gmail.com>
2025-03-02 15:41:23 +0100

    refactor: menu layout

--- src/menu.js
+++ src/menu.js
@@ -4,63 +4,73 @@ import homeImage from "../dist/images/Vector\ \(1\).svg";
 const menu = () => {
     let div = document.querySelector('#content');
 
-    let homePage = document.querySelector('.home-page');
-    div.removeChild(homePage);
+    let homeHeader = document.querySelector('.home-header');
+    div.removeChild(homeHeader);
+    let homeMain = document.querySelector('.home-main');
+    div.removeChild(homeMain);
+    let homeFooter = document.querySelector('.home-footer');
+    div.removeChild(homeFooter);
 
-    let menuPage = document.createElement('section');
-    menuPage.classList.add('menu-page');
-    div.appendChild(menuPage);
+    /* Header */
+    let menuHeader = document.createElement('header');
+    menuHeader.classList.add('menu-header');
+    div.appendChild(menuHeader);
 
-    let titleMenu = document.createElement('h1');
-    titleMenu.textContent = 'Weekly menu';
-    titleMenu.classList.add('title-menu');
-    menuPage.appendChild(titleMenu);
+    let menuHeaderTitle = document.createElement('h1');
+    menuHeaderTitle.textContent = 'Weekly menu';
+    menuHeaderTitle.classList.add('menu-title');
+    menuHeader.appendChild(menuHeaderTitle);
 
     let homeButton = document.createElement('img');
     homeButton.src = homeImage;
     homeButton.classList.add('home-button');
-    menuPage.appendChild(homeButton);
+    menuHeader.appendChild(homeButton);
 
     homeButton.addEventListener('click', () => {
-        let menuPage = document.querySelector('.menu-page');
-        div.removeChild(menuPage);
+        let menuHeader = document.querySelector('.menu-header');
+        div.removeChild(menuHeader);
+        let menuMain = document.querySelector('.menu-main');
+        div.removeChild(menuMain);
+        let menuFooter = document.querySelector('.menu-footer');
+        div.removeChild(menuFooter);
 
         loadHome();
     });
 
-
-    let imageSectionMenu = document.createElement('section');
-    imageSectionMenu.classList.add('image-section-menu');
-    menuPage.appendChild(imageSectionMenu);
+    /* Main */
+    let menuMain = document.createElement('main');
+    menuMain.classList.add('menu-main');
+    div.appendChild(menuMain);
 
     let plate1 = document.createElement('p');
     plate1.textContent = 'Pad Thai - 11$';
     plate1.classList.add('plate-1');
-    imageSectionMenu.appendChild(plate1);
+    menuMain.appendChild(plate1);
 
     /* ready if i want to add img
     let img1 = document.createElement('img');
     img1.classList.add('img1');
-    imageSectionMenu.appendChild(img1);*/
+    menuMain.appendChild(img1);*/
 
     let plate2 = document.createElement('p');
     plate2.textContent = 'Korean BBQ - 15$';
     plate2.classList.add('plate-2');
-    imageSectionMenu.appendChild(plate2);
+    menuMain.appendChild(plate2);
 
     let plate3 = document.createElement('p');
     plate3.textContent = 'Udon - 14$';
     plate3.classList.add('plate-3');
-    imageSectionMenu.appendChild(plate3);
+    menuMain.appendChild(plate3);
 
     let plate4 = document.createElement('p');
     plate4.textContent = 'Pekin Duck - 18$';
     plate4.classList.add('plate-4');
-    imageSectionMenu.appendChild(plate4);
+    menuMain.appendChild(plate4);
 
+    /* Footer */
     let menuFooter = document.createElement('footer');
-    menuFooter.classList.add('menu-footer')
-    menuPage.appendChild(menuFooter);
+    menuFooter.classList.add('menu-footer');
+    div.appendChild(menuFooter);
 
     let titleFooter = document.createElement('p');
     titleFooter.textContent = 'Contact info';
@@ -73,17 +83,14 @@ const menu = () => {
 
     let gmailText = document.createElement('p');
     gmailText.textContent = '1234@gmail.com';
-    gmailText.classList.add('gmail-text');
     infoSection.appendChild(gmailText);
 
     let phoneNum = document.createElement('p');
     phoneNum.textContent = '+050 250 8541';
-    phoneNum.classList.add('phone-num');
     infoSection.appendChild(phoneNum);
 
     let addressText = document.createElement('p');
     addressText.textContent = '36 Road, 152 ExCity, JPN';
-    addressText.classList.add('address-text');
     infoSection.appendChild(addressText);
 }
 
